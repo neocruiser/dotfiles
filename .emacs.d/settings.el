31d177e011fc5f5ca6a538645a7f86fb7db25ce8
neocruiser <slei.bass@gmail.com>
2014-11-25 12:27:36 -0500

    update org-mode settings

--- .emacs.d/settings.el
+++ .emacs.d/settings.el
@@ -164,7 +164,7 @@
   (progn
     (diminish 'flyspell-mode "FS")))
 
-;; remove/activate spelling in orgmode
+;; remove/activate spelling in org-mode
 (dolist (hook '(text-mode-hook org-mode-hook))
   (add-hook hook (lambda () (flyspell-mode 1))))
 (dolist (hook '(change-log-mode-hook log-edit-mode-hook org-agenda-mode-hook))
@@ -358,48 +358,348 @@ file to write to."
 (define-key lisp-interaction-mode-map (kbd "C-c C-z") 'ielm-other-window)
 
 
-;;; ORG mode
-;; setting up the staging area with DROPBOX
-(setq org-mobile-directory "/media/Data/Dropbox/Private/org/")
-;    (global-set-key "\C-cc" 'org-capture)
-
-(setq org-todo-keywords
-           '((sequence "TODO(t)" "CODE(o)" "IDEA(i)" "REMINDER(r@)" "|" "CANCELED(c)" "DONE(d!)")))
-(setq org-todo-keyword-faces
-           '(("TODO" . org-warning) ("IDEA" . "yellow") ("CODE" . "blue")
-             ("CANCELED" . (:foreground "brown" :weight bold))))
-;; TODO ORG mode customization http://tinyurl.com/m3fs7kt
-
-(setq org-clock-persist 'history)
-     (org-clock-persistence-insinuate)
-(setq org-log-done 'time)
-(setq org-log-done 'note)
-;; SAve the clock history across Emacs sessions
 
 ;; Turns on word wrapping (ONLY in org mode
-(define-key org-mode-map "\M-q" 'toggle-truncate-lines)
 
 
-(setq org-agenda-files (list "/media/Data/Dropbox/Private/org/grymoire.org"))
 
 ;; tags
-(setq org-tag-alist '(
-  ("@work" . ?w) ("@home" . ?h) ("perl" . ?p) 
-  ("sysadmin" . ?y) ("R" . ?r) ("latex" . ?l)
-  ("ML" . ?m) ("rstat" ?t) ("loisir" . ?i)
-  ("emacs" . ?c) ("Table" . ?s)
-))
 
-;; PDFs visited in Org-mode are opened in Evince (and not in the default choice) http://stackoverflow.com/a/8836108/789593
-(add-hook 'org-mode-hook
-      '(lambda ()
-         (delete '("\\.pdf\\'" . default) org-file-apps)
-         (add-to-list 'org-file-apps '("\\.pdf\\'" . "evince %s"))))
 
-;;; Org-toc
-(use-package org-toc
-  :disabled t
-  :init (add-hook 'org-mode-hook 'org-toc-enable))
+
+
+
+(defun my/load-github-flavored-markdown ()
+  (interactive)
+  (when (file-exists-p "~/.emacs.d/ox-gfm.el")
+    (load-file "~/.emacs.d/ox-gfm.el")))
+
+(use-package org
+  :bind (("C-c l" . org-store-link)
+         ("C-c a" . org-agenda)
+         ("C-c b" . org-iswitchb)
+         ("C-c c" . org-capture)
+         ("C-c M-p" . org-babel-previous-src-block)
+         ("C-c M-n" . org-babel-next-src-block)
+         ("C-c S" . org-babel-previous-src-block)
+         ("C-c s" . org-babel-next-src-block))
+  :config
+  (progn
+    (use-package org-install)
+    ;; load github-flavored-markdown
+    (add-hook 'org-mode-hook 'turn-on-auto-fill)
+    (setq org-directory "/media/Data/Dropbox/Private/org"
+          org-startup-indented t
+          org-return-follows-link t
+          ;; allow changing between todo stats directly by hotkey
+          org-use-fast-todo-selection t
+          org-src-fontify-natively t
+          org-fontify-whole-heading-line t
+          org-completion-use-ido t
+          org-edit-src-content-indentation 0
+          ;; Imenu should use 3 depth instead of 2
+          org-imenu-depth 3
+          org-agenda-start-on-weekday nil
+          ;; Use sticky agenda's so they persist
+          org-agenda-sticky t
+          ;; show 4 agenda days
+          org-agenda-span 4
+          org-special-ctrl-a/e t
+          org-special-ctrl-k t
+          org-yank-adjusted-subtrees nil
+          org-src-window-setup 'current-window
+          ;; Overwrite the current window with the agenda
+          org-agenda-window-setup 'current-window
+          ;; Use full outline paths for refile targets - we file directly with IDO
+          org-refile-use-outline-path t
+          ;; Targets complete directly with IDO
+          org-outline-path-complete-in-steps nil
+          ;; Allow refile to create parent tasks with confirmation
+          org-refile-allow-creating-parent-nodes (quote confirm)
+          ;; Use IDO for both buffer and file completion and ido-everywhere to t
+          ido-everywhere t
+          ido-max-directory-size 100000
+          ;; always enable noweb, results as code and exporting both
+          org-babel-default-header-args
+          (cons '(:noweb . "yes")
+                (assq-delete-all :noweb org-babel-default-header-args))
+          org-babel-default-header-args
+          (cons '(:exports . "both")
+                (assq-delete-all :exports org-babel-default-header-args))
+          ;; I don't want to be prompted on every code block evaluation
+          org-confirm-babel-evaluate nil
+          ;; Compact the block agenda view
+          org-agenda-compact-blocks t
+          ;; Mark entries as done when archiving
+          org-archive-mark-done nil
+          org-archive-location "%s_archive::* Archived Tasks"
+          ;; Sorting order for tasks on the agenda
+          org-agenda-sorting-strategy
+          (quote ((agenda habit-down
+                          time-up
+                          priority-down
+                          user-defined-up
+                          effort-up
+                          category-keep)
+                  (todo priority-down category-up effort-up)
+                  (tags priority-down category-up effort-up)
+                  (search priority-down category-up)))
+
+          ;; Enable display of the time grid so we can see the marker for the current time
+          org-agenda-time-grid (quote ((daily today remove-match)
+                                       #("----------------" 0 16 (org-heading t))
+                                       (0900 1100 1300 1500 1700)))
+          org-agenda-include-diary t
+          org-agenda-insert-diary-extract-time t
+          org-agenda-repeating-timestamp-show-all t
+          ;; Show all agenda dates - even if they are empty
+          org-agenda-show-all-dates t)
+
+    ;; PDFs visited in Org-mode are opened in Evince (and not in the default choice) http://stackoverflow.com/a/8836108/789593
+    (add-hook 'org-mode-hook
+              '(lambda ()
+                 (delete '("\\.pdf\\'" . default) org-file-apps)
+                 (add-to-list 'org-file-apps '("\\.pdf\\'" . "evince %s"))))
+
+    (use-package org-toc
+      :disabled t
+      :init (add-hook 'org-mode-hook 'org-toc-enable))
+
+    ;; Agenda org-mode files
+    (setq org-agenda-files
+          '("/media/Data/Dropbox/Private/org/grymoire.org"
+            "/media/Data/Dropbox/Private/org/human/human.org"
+            ))
+    ;; Org todo keywords
+    (setq org-todo-keywords
+          (quote
+           ((sequence "SOMEDAY(s)" "TODO(t)" "INPROGRESS(i)" "WAITING(w@/!)" "NEEDSREVIEW(n@/!)"
+                      "|" "DONE(d)")
+            (sequence "WAITING(w@/!)" "HOLD(h@/!)"
+                      "|" "CANCELLED(c@/!)"))))
+    ;; Org faces
+    (setq org-todo-keyword-faces
+          (quote (("TODO" :foreground "red" :weight bold)
+                  ("INPROGRESS" :foreground "deep sky blue" :weight bold)
+                  ("SOMEDAY" :foreground "purple" :weight bold)
+                  ("NEEDSREVIEW" :foreground "#edd400" :weight bold)
+                  ("DONE" :foreground "forest green" :weight bold)
+                  ("WAITING" :foreground "orange" :weight bold)
+                  ("HOLD" :foreground "magenta" :weight bold)
+                  ("CANCELLED" :foreground "forest green" :weight bold))))
+    ;; add or remove tags on state change
+    (setq org-todo-state-tags-triggers
+          (quote (("CANCELLED" ("CANCELLED" . t))
+                  ("WAITING" ("WAITING" . t))
+                  ("HOLD" ("WAITING") ("HOLD" . t))
+                  (done ("WAITING") ("HOLD"))
+                  ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
+                  ("INPROGRESS" ("WAITING") ("CANCELLED") ("HOLD"))
+                  ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
+    ;; refile targets all level 1 headers in todo.org and notes.org
+    (setq org-refile-targets '((nil :maxlevel . 2)
+                               (org-agenda-files :maxlevel . 2)))
+    ;; quick access to common tags
+    (setq org-tag-alist
+          '(("@work" . ?w)
+            ("@home" . ?h)
+            ("perl" . ?p)
+            ("sysadmin" . ?y)
+            ("R" . ?r)
+            ("latex" . ?l)
+            ("ML" . ?m)
+            ("rstat" ?t)
+            ("loisir" . ?i)
+            ("emacs" . ?c)
+            ("Table" . ?s)
+            ("export" . ?e)
+            ("noexport" . ?n)))
+    ;; Custom agenda command definitions
+    (setq org-agenda-custom-commands
+          (quote
+           (("N" "Notes" tags "NOTE"
+             ((org-agenda-overriding-header "Notes")
+              (org-tags-match-list-sublevels t)))
+            (" " "Agenda"
+             ((agenda "" nil)
+              ;; All items with the "REFILE" tag, everything in refile.org
+              ;; automatically gets that applied
+              (tags "REFILE"
+                    ((org-agenda-overriding-header "Tasks to Refile")
+                     (org-tags-match-list-sublevels nil)))
+              ;; All "INPROGRESS" todo items
+              (todo "INPROGRESS"
+                    ((org-agenda-overriding-header "Current work")))
+              ;; All headings with the "support" tag
+              (tags "support/!"
+                    ((org-agenda-overriding-header "Support cases")))
+              ;; All "NEESREVIEW" todo items
+              (todo "NEEDSREVIEW"
+                    ((org-agenda-overriding-header "Waiting on reviews")))
+              ;; All "WAITING" items without a "support" tag
+              (tags "WAITING-support"
+                    ((org-agenda-overriding-header "Waiting for feedback")))
+              ;; All TODO items
+              (todo "TODO"
+                    ((org-agenda-overriding-header "Task list")
+                     (org-agenda-sorting-strategy '(category-keep))))
+              ;; Everything on hold
+              (todo "HOLD"
+                    ((org-agenda-overriding-header "On-hold")))
+              ;; Everything that's done and archivable
+              (todo "DONE"
+                    ((org-agenda-overriding-header "Tasks for archive")
+                     (org-agenda-skip-function 'my/skip-non-archivable-tasks))))
+             nil))))
+
+    (ido-mode (quote both))
+
+    ;; Exclude DONE state tasks from refile targets
+    (defun my/verify-refile-target ()
+      "Exclude todo keywords with a done state from refile targets"
+      (not (member (nth 2 (org-heading-components)) org-done-keywords)))
+    (setq org-refile-target-verify-function 'my/verify-refile-target)
+
+    (define-key org-mode-map "\M-q" 'toggle-truncate-lines)
+    (define-key org-mode-map (kbd "C-M-<return>") 'org-insert-todo-heading)
+    (define-key org-mode-map (kbd "C-c t") 'org-todo)
+    (define-key org-mode-map (kbd "M-G") 'org-plot/gnuplot)
+    (local-unset-key (kbd "M-S-<return>"))
+
+    (add-hook 'org-mode-hook
+              (lambda ()
+                (turn-on-flyspell)
+                (define-key org-mode-map [C-tab] 'other-window)
+                (define-key org-mode-map [C-S-tab]
+                  (lambda ()
+                    (interactive)
+                    (other-window -1)))
+                (define-key org-mode-map (kbd "C-'")
+                  'eyebrowse-next-window-config)))
+
+    ;; org-babel stuff
+    (org-babel-do-load-languages
+     'org-babel-load-languages
+     '((emacs-lisp . t)
+       (dot . t)
+       (sh . t)
+       (R . t)
+       (perl . t)
+       (gnuplot . t)
+       (latex . t)))
+
+    ;; Use org.css from the :wq website for export document stylesheets
+    (setq org-html-head-extra
+          "<link rel=\"stylesheet\" href=\"http://dakrone.github.io/org.css\" type=\"text/css\" />")
+    (setq org-html-head-include-default-style nil)
+
+
+    ;; ensure this variable is defined
+    (unless (boundp 'org-babel-default-header-args:sh)
+      (setq org-babel-default-header-args:sh '()))
+
+    ;; add a default shebang header argument shell scripts
+    (add-to-list 'org-babel-default-header-args:sh
+                 '(:shebang . "#!/usr/bin/env zsh"))
+
+
+    ;; Function declarations
+    (defun my/skip-non-archivable-tasks ()
+      "Skip trees that are not available for archiving"
+      (save-restriction
+        (widen)
+        ;; Consider only tasks with done todo headings as archivable candidates
+        (let ((next-headline (save-excursion
+                               (or (outline-next-heading) (point-max))))
+              (subtree-end (save-excursion (org-end-of-subtree t))))
+          (if (member (org-get-todo-state) org-todo-keywords-1)
+              (if (member (org-get-todo-state) org-done-keywords)
+                  (let* ((daynr (string-to-int
+                                 (format-time-string "%d" (current-time))))
+                         (a-month-ago (* 60 60 24 (+ daynr 1)))
+                         (this-month
+                          (format-time-string "%Y-%m-" (current-time)))
+                         (subtree-is-current
+                          (save-excursion
+                            (forward-line 1)
+                            (and (< (point) subtree-end)
+                                 (re-search-forward this-month
+                                                    subtree-end t)))))
+                    (if subtree-is-current
+                        subtree-end     ; Has a date in this month, skip it
+                      nil))             ; available to archive
+                (or subtree-end (point-max)))
+            next-headline))))
+
+    (defun my/save-all-agenda-buffers ()
+      "Function used to save all agenda buffers that are
+currently open, based on `org-agenda-files'."
+      (interactive)
+      (save-current-buffer
+        (dolist (buffer (buffer-list t))
+          (set-buffer buffer)
+          (when (member (buffer-file-name)
+                        (mapcar 'expand-file-name (org-agenda-files t)))
+            (save-buffer)))))
+
+    ;; save all the agenda files after each capture
+    (add-hook 'org-capture-after-finalize-hook 'my/save-all-agenda-buffers)
+
+    (use-package org-id
+      :config
+      (progn
+        (setq org-id-link-to-org-use-id t)
+
+        (defun my/org-custom-id-get (&optional pom create prefix)
+          "Get the CUSTOM_ID property of the entry at point-or-marker POM.
+If POM is nil, refer to the entry at point. If the entry does not
+have an CUSTOM_ID, the function returns nil. However, when CREATE
+is non nil, create a CUSTOM_ID if none is present already. PREFIX
+will be passed through to `org-id-new'. In any case, the
+CUSTOM_ID of the entry is returned."
+          (interactive)
+          (org-with-point-at pom
+            (let ((id (org-entry-get nil "CUSTOM_ID")))
+              (cond
+               ((and id (stringp id) (string-match "\\S-" id))
+                id)
+               (create
+                (setq id (org-id-new prefix))
+                (org-entry-put pom "CUSTOM_ID" id)
+                (org-id-add-location id (buffer-file-name (buffer-base-buffer)))
+                id)))))
+
+        (defun my/org-add-ids-to-headlines-in-file ()
+          "Add CUSTOM_ID properties to all headlines in the
+current file which do not already have one."
+          (interactive)
+          (org-map-entries (lambda () (my/org-custom-id-get (point) 'create))))
+
+        ;; automatically add ids to captured headlines
+        (add-hook 'org-capture-prepare-finalize-hook
+                  (lambda () (my/org-custom-id-get (point) 'create)))))
+
+    (defun my/org-inline-css-hook (exporter)
+      "Insert custom inline css to automatically set the
+background of code to whatever theme I'm using's background"
+      (when (eq exporter 'html)
+        (let* ((my-pre-bg (face-background 'default))
+               (my-pre-fg (face-foreground 'default)))
+          ;;(setq org-html-head-include-default-style nil)
+          (setq
+           org-html-head-extra
+           (concat
+            org-html-head-extra
+            (format "<style type=\"text/css\">\n pre.src {background-color: %s; color: %s;}</style>\n"
+                    my-pre-bg my-pre-fg))))))
+
+    (add-hook 'org-export-before-processing-hook 'my/org-inline-css-hook)
+
+    ))
+
+
+
+
 
 
 
